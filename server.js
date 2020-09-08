d1ad3e299404a60ea388d90ce28bd54851a41339
David Reke <david.e.reke@gmail.com>
2020-09-08 13:06:25 -0400

    re-ran build

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client/build'))
+app.use(express.static('client/build'));
 
 const uri = process.env.ATLAS_URI;
         
